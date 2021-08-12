ffa42d9bee2fa03a3d39db0427ec865a2d66db49
andrey <pugach2012@bk.ru>
2021-08-12 09:29:05 +0300

    Rewritten on persistence

--- dao/clickhouse/migrations/004_transfers.up.sql
+++ dao/clickhouse/migrations/004_transfers.up.sql
@@ -2,8 +2,8 @@ create table transfers
 (
     trf_id         FixedString(40),
     trf_tx_hash    FixedString(64),
-    trf_from       FixedString(45),
-    trf_to         FixedString(45),
+    trf_from       FixedString(64),
+    trf_to         FixedString(64),
     trf_amount     Decimal128(18),
     trf_created_at DateTime,
     trf_currency   String
